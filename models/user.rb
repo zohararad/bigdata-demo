4a1e1d2a13d915e996902e434c4f4c8d87bdd04c
Zohar Arad <zohar@zohararad.com>
2013-02-06 09:57:55 +0200

    added indexes to user model

--- models/user.rb
+++ models/user.rb
@@ -9,9 +9,7 @@ class User
   field :skills,    type: Array
 
   embeds_many :employments
-  # You can define indexes on documents using the index macro:
-  # index :field <, :unique => true>
 
-  # You can create a composite key in mongoid to replace the default id using the key macro:
-  # key :field <, :another_field, :one_more ....>
+  index({ friends: 1 }, { unique: false })
+  index({ skills: 1 }, { unique: false })
 end
