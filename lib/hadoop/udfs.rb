ffe81e4d0b69cb5a49ec46797ce9837e22f9ba13
Zohar Arad <zohar@zohararad.com>
2013-02-14 10:45:40 +0200

    added pig script for hourly search history

--- lib/hadoop/udfs.rb
+++ lib/hadoop/udfs.rb
@@ -2,25 +2,9 @@ require 'pigudf'
 
 class Udfs < PigUdf
 
-  #outputSchema "relative_position:int"
-  def group_by_day(timestamps)
-    keywords = {}
-    timestamps.each do |ts|
-      kw, ts = ts.first, ts.last
-      keywords[kw] ||= {}
-      t = Time.at(ts.to_i).strftime('%Y%m%d%H00')
-      keywords[kw][t] ||= 0
-      keywords[kw][t] += 1
-    end
-    result = keywords.values.first.map{|k, v|
-      {:hour => k, :count => v}
-    }
-    puts "======================"
-    puts result.inspect
-    result
-  end
-
+  #outputSchema "timestamp:int"
   def hour(timestamp)
-    Time.at(timestamp.to_i).strftime('%Y%m%d%H00')
+    t = Time.at(timestamp.to_i)
+    Time.utc(t.year, t.month, t.day, t.hour, 0, 0).to_i
   end
 end
